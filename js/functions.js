df60879401183f0db828b783d9e91b00c9e85433
tarun1475 <tarunkumargupta14@gmail.com>
2018-09-25 17:29:24 +0530

    all changes

---
+++ js/functions.js
@@ -0,0 +1,374 @@
+function createAccount(){
+	window.location = "createAccount.html";
+}
+
+function goToProfile(){
+  window.location = "profile.html";
+}
+
+// function loginUser(){
+// 	var privateLoginKey = $("#privateLoginKey").val();
+
+// 	var accountKeys = JSON.parse(localStorage.accountKeys);
+
+// 	if(accountKeys.mnemonic == privateLoginKey){
+// 		window.location = "home.html";
+// 	}
+
+// 	else{
+
+// 		alert("Wrong Key");
+// 	}
+// }
+
+function login(){
+	localStorage.clear();
+	var privateSeed = $("#privateSeed").val();
+
+	var keys = getKeyPair(privateSeed);
+
+	var user = {
+		"publicKey":keys.publicKey,
+		"privateKeyHash":keys.privateKeyHash
+	};
+
+
+	 $.ajax({
+        url: 'http://159.65.153.3:7001/recovery_key/login_user',
+        type:'POST',
+        data: user ,
+        dataType : "json",
+        success: function (response) {
+         if(response.data){
+         	localStorage.setItem("accountKeys",JSON.stringify(keys));
+         	window.location = "index.html";
+         } 
+        },
+        error: function () {
+           
+        }
+    });
+}
+
+function logoutUser(){
+	localStorage.clear();
+	window.location = "register.html";
+}
+
+var userFriends = [];
+var friendsCount = 0;
+
+function addFriend(){
+	var publicKey = $("#addFriend").val();
+	var user  =  {
+  		"user_public_key":publicKey
+		};
+
+
+	 $.ajax({
+        url: 'http://159.65.153.3:7001/recovery_key/fetch_user_public_key',
+        type:'GET',
+        data: user ,
+        dataType : "json",
+        success: function (response) {
+          if(response.data)  {
+          	userFriends.push(response.data.user_public_key);
+          	friendsCount++;
+
+          	 if(friendsCount <=3){
+          	 document.getElementById("friendsList").innerHTML +=   '<div class="card-panel">'+
+            '<span id="friendsPublicKey" class="blue-text text-darken-2">'+response.data.user_public_key+'</span>'+
+          '<br />Added</div>';
+
+          	 }
+
+          	if(friendsCount == 3) {
+          	console.log(userFriends);
+			   localStorage.setItem("userFriends",JSON.stringify(userFriends));
+				 document.getElementById("friendsList").innerHTML += '<a onclick="sendSecret()" class="waves-effect waves-light btn-large">Send Secret</a>';
+			}
+          }
+          
+        },
+        error: function () {
+           
+        }
+    });
+
+
+
+}
+
+function Friend(){
+	var publicKey = $("#addFriend").val();
+	var user  =  {
+  		"user_public_key":publicKey
+		};
+
+
+	 $.ajax({
+        url: 'http://159.65.153.3:7001/recovery_key/fetch_user_public_key',
+        type:'GET',
+        data: user ,
+        dataType : "json",
+        success: function (response) {
+          if(response.data)  {
+			userFriends.push(response.data.user_public_key);
+          	friendsCount++;
+
+          	 if(friendsCount <=3){
+          	 	 document.getElementById("friendsList").innerHTML += '<div class="card-panel">'+
+            '<span id="friendsPublicKey" class="blue-text text-darken-2">'+response.data.user_public_key+'</span>'+
+          '<br />Added</div>';
+
+          	 }
+
+          	if(friendsCount == 3) {
+			localStorage.setItem("userFriends",JSON.stringify(userFriends));
+			  document.getElementById("friendsList").innerHTML += '<a onclick="sendSecretRequest()" class="waves-effect waves-light btn-large">Send Secret</a>';
+      }
+          }
+          
+        },
+        error: function () {
+           
+        }
+    });
+
+
+
+}
+
+//function to send secret to friends
+
+function sendSecret(){
+	/**
+	trustData = [
+	{
+		"user_id":user_id,
+		"encrypted_key_data":data
+	},
+	{
+		"user_id":user_id,
+		"encrypted_key_data":data
+	},
+	{
+		"user_id":user_id,
+		"encrypted_key_data":data
+	}
+	];
+	**/
+
+	var userFriends = JSON.parse(localStorage.userFriends);
+
+	var secretShares = getSecretParts();
+
+	var trustData = [];
+
+	for(i=0 ; i<3 ; i++){
+		var dataObject = {};
+		dataObject.user_public_key = userFriends[i];
+		dataObject.encrypted_key_data = getEncrytedKeydata(userFriends[i],secretShares[i]);
+		trustData.push(dataObject);
+	}
+
+	var  user = {
+		"trust_data":trustData
+	};
+
+	console.log(user);
+
+	 $.ajax({
+        url: 'http://159.65.153.3:7001/recovery_key/user_trust_data',
+        type:'POST',
+        data: user ,
+        dataType : "json",
+        success: function (response) {
+           document.getElementById("secretResult").innerHTML = "<br />Shared Successfully!!";
+           localStorage.setItem("friendsAdded",1);
+
+           setTimeout(function(){
+           	window.location = "index.html";
+           },1500);
+        },
+        error: function () {
+           
+        }
+    });
+}
+
+
+//function to send otp  through email
+
+function sendOtp(){
+	var email = $('#email').val();
+
+	var  user = {
+		"user_email":email
+	};
+
+	 $.ajax({
+        url: 'http://159.65.153.3:7001/recovery_key/send_otp',
+        type:'POST',
+        data: user ,
+        dataType : "json",
+        success: function (response) {
+           if(response.session_id){
+           	 localStorage.setItem("session_id",response.session_id);
+           	 localStorage.setItem("email",email);
+           } 
+           window.location = "verify.html";
+        },
+        error: function () {
+           
+        }
+    });
+
+}
+
+//function to send otp  through email
+
+function sendRecoveryOtp(){
+	var email = $('#email').val();
+
+	var  user = {
+		"user_email":email
+	};
+
+	 $.ajax({
+        url: 'http://159.65.153.3:7001/recovery_key/send_recovery_otp',
+        type:'POST',
+        data: user ,
+        dataType : "json",
+        success: function (response) {
+           if(response.session_id){
+           	 localStorage.setItem("session_id",response.session_id);
+           	 localStorage.setItem("email",email);
+           } 
+           window.location = "verifyRecovery.html";
+        },
+        error: function () {
+           
+        }
+    });
+
+}
+
+function verifyOtp(){
+	var otp = $('#otp').val();
+
+	var accountKeys = JSON.parse(localStorage.accountKeys);
+
+	var  user = {
+		"otp":otp,
+		"session_id":localStorage.session_id,
+		"email":localStorage.email,
+		"publicKey":accountKeys.publicKey
+	};
+
+	 $.ajax({
+        url: 'http://159.65.153.3:7001/recovery_key/verify_otp',
+        type:'POST',
+        data: user ,
+        dataType : "json",
+        success: function (response) {
+           localStorage.setItem("userDetails", JSON.stringify(response.userDetails));
+           localStorage.setItem("emailVerified",1);
+           window.location = "friends.html";
+        },
+        error: function () {
+           
+        }
+    });
+
+}
+
+function verifyRecoveryOtp(){
+	var otp = $('#otp').val();
+
+	var  user = {
+		"otp":otp,
+		"session_id":localStorage.session_id,
+		"email":localStorage.email
+	};
+
+	 $.ajax({
+        url: 'http://159.65.153.3:7001/recovery_key/verify_recovery_otp',
+        type:'POST',
+        data: user ,
+        dataType : "json",
+        success: function (response) {
+        	localStorage.setItem("accountDetails",JSON.stringify(response.userDetails));
+
+        	var newAccountKeys = getNewKeys();
+
+			localStorage.setItem("newAccountKeys",JSON.stringify(newAccountKeys));
+
+			window.location = "addFriends.html";
+          
+        },
+        error: function () {
+           
+        }
+    });
+
+}
+
+// //function to send secret to friends
+
+function sendSecretRequest(){
+	/**
+	trustData = [
+	{
+		"user_id":user_id,
+		"encrypted_key_data":data
+	},
+	{
+		"user_id":user_id,
+		"encrypted_key_data":data
+	},
+	{
+		"user_id":user_id,
+		"encrypted_key_data":data
+	}
+	];
+	**/
+
+	var userFriends = JSON.parse(localStorage.userFriends);
+	var newAccountKeys =  JSON.parse(localStorage.newAccountKeys);
+	var accountDetails =  JSON.parse(localStorage.accountDetails);
+
+	var trustData = [];
+
+	for(i=0 ; i<3 ; i++){
+		var dataObject = {};
+		dataObject.user_public_key = userFriends[i];
+		trustData.push(dataObject);
+	}
+
+	var  user = {
+		"publicKey":accountDetails.user_public_key,
+		"newPublicKey":newAccountKeys.publicKey,
+		"trust_data":trustData
+	};
+
+	console.log(newAccountKeys.privateKey);
+
+	 $.ajax({
+        url: 'http://159.65.153.3:7001/recovery_key/user_recovery_trust_data',
+        type:'POST',
+        data: user ,
+        dataType : "json",
+        success: function (response) {
+           window.location = "request.html";
+
+  
+        },
+        error: function () {
+           
+        }
+    });
+}
+
+
+
---
+++ js/preloader.js
@@ -0,0 +1,214 @@
+function checkUser(){
+    if(localStorage.request_id != undefined) window.location = "request.html";
+}
+
+function fetchRecoveryRequest(){
+	
+	var accountDetails =  JSON.parse(localStorage.accountDetails);
+
+	var user  =  {
+  		"publicKey":accountDetails.user_public_key
+		};
+
+	 $.ajax({
+        url: 'http://159.65.153.3:7001/recovery_key/fetch_recovery_trust_data',
+        type:'POST',
+        data: user ,
+        dataType : "json",
+        success: function (response) {
+            var count = 0;
+
+            localStorage.setItem("request_id",response.result[0].request_id);
+
+            console.log(response);
+             document.getElementById("recoveryRequestDetails").innerHTML = '<div><b>Request Id: </b>'+response.result[0].request_id+'</p></div>';
+             var secrets = [];
+            for(m = 0 ; m < response.result.length  ; m++){
+                var approved = "";
+                if(response.result[m].trust_data) {
+                count++;
+                var trustData = getRecoveryDecrypteddata(response.result[m].trust_data);
+                approved = 'Approved Status: Done!'; 
+                secrets.push(trustData);
+                }
+                else {
+                    approved = 'Not Approved Yet';
+                }
+
+                document.getElementById("recoveryRequestDetails").innerHTML +='<div class="card"><div class="card-content">'+
+
+                      '<p id="friendsPublicKey">User: ' + response.result[m].user_public_key + '</p><br />' +
+                      'Date: ' + response.result[m].logged_on.substring(0,10) + '<br />'+
+                      approved+
+                      '</div></div>';
+
+            }
+        	// var secrets = [];
+
+        	// for(i = 0 ; i < response.result.length; i++){
+
+        	// 	var trustData = getRecoveryDecrypteddata(response.result[i].trust_data);
+        	// 	secrets.push(trustData);
+        	// }
+        	 
+
+             if(count >= 3) {
+                var secretRecovery = getOriginalMnemonic(secrets);
+                document.getElementById("recoveryRequestDetails").innerHTML = '<div class="card"><div class="card-content">'+
+                      '<h3>Mnemonic</h3> <br />'+'<b>You have successfully recovered your mnemonic phrase!</b> <br />'+
+                      secretRecovery+
+                      '</div></div>';
+             }
+
+        	 
+        },
+        error: function () {
+           
+        }
+    });
+
+
+}
+
+
+
+
+function getTrustRequests(){
+	var accountKeys = JSON.parse(localStorage.accountKeys);
+
+	var user  =  {
+  		"publicKey":accountKeys.publicKey
+		};
+
+	 $.ajax({
+        url: 'http://159.65.153.3:7001/recovery_key/fetch_recovery_data',
+        type:'POST',
+        data: user ,
+        dataType : "json",
+        success: function (response) {
+            console.log(response);
+            if(response.status == 0 || response.recoveryData.length == 0){
+                document.getElementById("trustDetails").innerHTML +='<div class="card"><div class="card-content">'+
+                      '<h4>No Requests Found!</h4>'+
+                      '</div></div>';
+            }
+
+            else {
+        	var trustData = [];
+        	var requestRecovery = [];
+
+        	for(i = 0 ; i < response.trustData.length ; i++){
+
+        		var data = getDecrypteddata(response.trustData[i].trust_data);
+            	data = JSON.parse(data);
+            	trustData.push(data);
+
+
+        	}
+
+
+
+        	for(j = 0 ; j < response.recoveryData.length ; j++){
+                var k = 0;
+        		while(k < trustData.length){
+        			var recoveryDetails = {};
+        			if(response.recoveryData[j].from_public_key == trustData[k].user_public_key) {
+                        var trust_data 					=   getEncrytedRecoverydata(response.recoveryData[j].new_public_key , trustData[k].secret );
+        				recoveryDetails.request_id      =	response.recoveryData[j].request_id;
+        				recoveryDetails.from_public_key =   response.recoveryData[j].from_public_key;
+        				recoveryDetails.new_public_key  =   response.recoveryData[j].new_public_key;
+        				recoveryDetails.secret			=   trust_data;
+        				recoveryDetails.logged_on		=   response.recoveryData[j].logged_on;
+                        
+                         requestRecovery.push(recoveryDetails);
+        			}
+
+                    k++;
+        		}
+
+        		
+        	}
+        	for(m = 0 ; m < requestRecovery.length  ; m++){
+
+        		localStorage.setItem("requestRecovery",JSON.stringify(requestRecovery));
+
+        		document.getElementById("trustDetails").innerHTML +='<div class="card"><div class="card-content">'+
+			          'Request Id: ' + requestRecovery[m].request_id  + '<br />' +
+			          '<p id="friendsPublicKey">From User: ' + requestRecovery[m].from_public_key + '</p><br />' +
+			          'Date: ' + requestRecovery[m].logged_on.substring(0,10) + '<br />'+
+			          '<a onclick="approveRecovery('+ m +')" class="waves-effect waves-light btn-large">Approve</a>'+
+                      '<div id="approveResult"></div>'+
+			          '</div></div>';
+
+        	}
+        }
+
+        },
+        error: function () {
+           
+        }
+    });
+
+
+}
+
+
+function approveRecovery(m){
+	var requestRecovery = JSON.parse(localStorage.requestRecovery);
+	var accountKeys     = JSON.parse(localStorage.accountKeys);
+
+	var user = {
+		"publicKey": accountKeys.publicKey,
+		"request_id":requestRecovery[m].request_id,
+		"trust_data":requestRecovery[m].secret
+	};
+
+	 $.ajax({
+        url: 'http://159.65.153.3:7001/recovery_key/update_recovery_trust_data',
+        type:'POST',
+        data: user ,
+        dataType : "json",
+        success: function (response) {
+            document.getElementById("approveResult").innerHTML = "Approved";
+            window.location = "";
+        },
+        error: function () {
+           
+        }
+    });
+
+}
+
+
+
+function generateKeys(){
+	var accountKeys = mnemonic();
+
+	localStorage.setItem("accountKeys",JSON.stringify(accountKeys));
+
+	var user  =  {
+  		"user_public_key":accountKeys.publicKey,
+  		"user_private_key_hash":accountKeys.privateKeyHash
+		};
+
+	 $.ajax({
+        url: 'http://159.65.153.3:7001/recovery_key/register_user',
+        type:'POST',
+        data: user ,
+        dataType : "json",
+        success: function (response) {
+            document.getElementById("mnemonic").innerHTML =   '<div class="card-panel">'+
+            '<span class="blue-text text-darken-2">'+ accountKeys.mnemonic+'</span>'+
+          '</div>';
+        },
+        error: function () {
+           
+        }
+    });
+
+	
+
+
+}
+
+ 
